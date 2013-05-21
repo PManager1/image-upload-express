6baf5c6005840ea6ad6d4aa210476e13f6bb8e0e
jpartee <jpca999@gmail.com>
2013-05-20 19:56:25 -0700

    upload working great

--- app.js
+++ app.js
@@ -12,14 +12,50 @@ var express = require('express')
 var app = express();
 
 
-// all environments
+app.configure(function() {
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-
 app.use(express.bodyParser());
 
+/*
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: __dirname + '/pictures' }));
+*/
+
+app.use(express.bodyParser({uploadDir:'./pictures'}));
+
+app.use(express.methodOverride());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+});
+
+
+
+app.get('/copy', function(req, res){
+  res.render("customer/create_copy");
+});
+
+
+// we need the fs module for moving the uploaded files
+var fs = require('fs');
+app.post('/file-upload', function(req, res) {
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+});
+
+
 
 
 app.get('/', home.index); 
@@ -32,7 +68,8 @@ app.post('/customer/create', customer.createCustomer);
 app.get('/customer/edit/:id', customer.edit); 
 app.post('/customer/edit/:id', customer.editCustomer); 
 
-
+app.delete('/customer/delete/:id', customer.delete); 
+// app.del('/customer/delete/:id', customer.deleteCustomer); 
 
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/customer.js
+++ routes/customer.js
@@ -14,7 +14,12 @@ exports.create = function (req,res) {
 };
 
 exports.createCustomer = function(req,res){
-	db.addCustomer({ name: req.body.name, email: req.body.email, telephone: req.body.telephone });
+		
+	console.dir(req.files.picture);
+
+	db.addCustomer({ name: req.body.name, email: req.body.email, telephone: req.body.telephone, 
+		picture: req.files.picture.path});
+
 	res.redirect('/customer');
 };
 
@@ -26,14 +31,26 @@ exports.details = function  (req,res) {
 exports.edit = function  (req,res) {
 	var customer = db.getCustomerById(req.params.id); 
 	res.render('customer/edit', {customer:customer});
-},
+};
 
 exports.editCustomer = function  (req,res) {
 	db.updateCustomer({ id:req.params.id, name:req.body.name, email:req.body.email, telephone:req.body.telephone });
 	res.redirect('/customer');
-}
+};
 
+exports.delete = function (req,res) {
+	 db.deleteCustomer(req.params.id); 
+	res.redirect('/customer');
+};
 
+exports.picture = function  (req,res) {
+	var customer = db.getCustomerById( req.params.id); 
+	if(req.query.attachment == 'true'){
+		res.download(customer.picture); 
+	}else {
+		res.sendfile(customer.picture);
+	}
+};
 
 
 
